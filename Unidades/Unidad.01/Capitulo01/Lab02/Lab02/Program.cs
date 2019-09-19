dd6fd43f57e7b1534aebc662a9488d0d1a80c9a6
nicolasf96 <nico.fierro1@gmail.com>
2019-09-19 11:34:47 -0300

    Agrego Lab.02 Capitulo 1 Unidad 1

---
+++ Unidades/Unidad.01/Capitulo01/Lab02/Lab02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string valor1 = "Esto es el valor 1";
+
+            int valor2 = 5;
+
+            string valor3 = valor1;
+
+            Console.WriteLine(valor1);
+
+            Console.WriteLine(valor2);
+
+            Console.WriteLine(valor3);
+
+            Console.ReadKey();
+
+        }
+    }
+}
